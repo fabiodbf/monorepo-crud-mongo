08d94af53fd20fbc70e47b56d9ab83014ed0e5da
Fabio Faria <fabiodbf@gmail.com>
2024-12-26 23:29:43 -0300

    Creating basic component Discarding webvitals for now

---
+++ WebApp/tasks/src/components/container.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./list";
+
+function Container() {
+  return (
+    <div>
+      <h1>Some title component here</h1>
+      <List />
+      <button>Some future buttom component here</button>
+    </div>
+  );
+}
+
+export default Container;
---
+++ WebApp/tasks/src/components/list.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Some task</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
