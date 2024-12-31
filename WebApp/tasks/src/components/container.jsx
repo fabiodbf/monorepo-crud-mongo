6cec7a693c3d7ba879ee07f6fbe48363e35e5d4c
Fabio Faria <fabiodbf@gmail.com>
2024-12-30 21:43:36 -0300

    Changing component to fetch tasks from API

--- WebApp/tasks/src/components/container.jsx
+++ WebApp/tasks/src/components/container.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import List from "./list";
+import { fetchTasks } from "../services/api";
 
 function Container() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const loadTasks = async () => {
+      const fetchedTasks = await fetchTasks();
+      setTasks(fetchedTasks);
+    };
+
+    loadTasks();
+  }, []);
+
   return (
     <div>
-      <h1>Some title component here</h1>
-      <List list={["batata"]} />
-      <button>Some future buttom component here</button>
+      <h1>Task List</h1>
+      <List list={tasks.map((task) => task.text)} />
+      <button>Some future button component here</button>
     </div>
   );
 }
