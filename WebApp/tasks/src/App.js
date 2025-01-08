db62dbcdd12d0bda005ad3269bc1233a63b08b24
Fabio Faria <fabiodbf@gmail.com>
2025-01-07 22:34:44 -0300

    Adding react-router-dom

--- WebApp/tasks/src/App.js
+++ WebApp/tasks/src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/global.css";
 import Container from "./components/container/container";
 
 function App() {
-  return <Container />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Container />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
